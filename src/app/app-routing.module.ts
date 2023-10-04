1c88202ae13dec56aa146a373e8ab6ce6f517ef1
Shailendra Pratap Singh <pratap.shailendra9@gmail.com>
2023-10-04 11:56:42 +0530

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './shared_component/dashboard/dashboard.component';
+import { HeaderComponent } from './shared_component/header/header.component';
+import { FooterComponent } from './shared_component/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'dashboard' , component:DashboardComponent},
+  {path :'header' , component:HeaderComponent},
+  {path :'footer' , component:FooterComponent},
+  {path :'', redirectTo:'dashboard' , pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
